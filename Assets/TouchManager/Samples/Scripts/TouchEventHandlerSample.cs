429ab4c56128b2864fc5457c1b10b39e7efa0116
sato-takashi <sato-takashi@KYCNT1203-16.local>
2013-12-02 20:58:10 +0900

    Handlerを実装中。多分まだ、不完全でコンパイルするとエラーが出るはず。

---
+++ Assets/TouchManager/Samples/Scripts/TouchEventHandlerSample.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchEventHandlerSample : MonoBehaviour {
+
+	public TouchEventHandler eventHandler;
+
+	void Awake () {
+		if(eventHandler == null){
+			eventHandler = GetComponent<TouchEventHandler>();
+		}
+	}
+
+	void OnEnable () {
+		eventHandler.OnTouch += Touch;
+		eventHandler.OnSwipeEnter += SwipeEnter;
+		eventHandler.OnSwipeStay += SwipeStay;
+		eventHandler.OnSwipeExit += SwipeExit;
+	}
+
+	void OnDisable () {
+		eventHandler.OnTouch -= Touch;
+		eventHandler.OnSwipeEnter -= SwipeEnter;
+		eventHandler.OnSwipeStay -= SwipeStay;
+		eventHandler.OnSwipeExit -= SwipeExit;
+	}
+
+
+	private void Touch(GameObject obj){
+		Debug.Log("Touch:" + obj.name);
+	}
+
+	private void SwipeEnter(GameObject obj){
+		Debug.Log("SwipeEnter:" + obj.name);
+	}
+
+	private void SwipeStay(GameObject obj){
+		Debug.Log("SwipeStay:" + obj.name);
+	}
+
+	private void SwipeExit(GameObject obj){
+		Debug.LogError("SwipeExit:" + obj.name);
+	}
+}
--- Assets/TouchManager/Scripts/TouchEventHandler.cs
+++ Assets/TouchManager/Scripts/TouchEventHandler.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+public enum TouchEventHandlerType{
+	NONE,
+	TOUCH,
+	SWIPE_ENTER,
+	SWIPE_STAY,
+	SWIPE_EXIT,
+}
+
 public class TouchEventHandler : MonoBehaviour {
 
 	public delegate void TouchDelegate(GameObject obj);
 	public event TouchDelegate OnTouch;
 
+	public delegate void SwipeDelegate(GameObject obj, Vector3 contactPosition);
+
+	public event SwipeDelegate OnSwipeEnter;
+	public event SwipeDelegate OnSwipeStay;
+	public event SwipeDelegate OnSwipeExit;
+
+	public event TouchDelegate OnLongPress;
+
+	public event TouchDelegate OnTap;
+	public event TouchDelegate OnLongTap;
+
 	public Camera touchCamera;
 
-	private Dictionary<GameObject,TouchEventType> _currentEvent;
-	private Dictionary<GameObject,TouchEventType> _prevEvent;
+	private Dictionary<GameObject,TouchEventHandlerType> _currentEventDict;
+	private Dictionary<GameObject,TouchEventHandlerType> _prevEventDict;
 
 	void OnEnable () {
 		TouchManager.OnTouch += Touch;
@@ -41,63 +60,111 @@ public class TouchEventHandler : MonoBehaviour {
 
 	}
 
-	void LastUpdate () {
-		_prevEvent = _currentEvent;
-		_currentEvent = new Dictionary<GameObject, TouchEventType>();
+	void LateUpdate () {
+		ProcessAfterTouchEvent();
+		_prevEventDict = _currentEventDict;
+		_currentEventDict = new Dictionary<GameObject, TouchEventHandlerType>();
 	}
 
 
 	private void Touch (Vector2 position) {
-
+		ProcessTouchEvent(TouchEventType.TOUCH,GetRaycastHit(position));
 	}
 
 	private void Press (Vector2 position, float pressTime) {
-
+		ProcessTouchEvent(TouchEventType.PRESS,GetRaycastHit(position));
 	}
 
 	private void Swipe (Vector2 position, Vector2 startPosition, float swipeTime) {
-
+		ProcessTouchEvent(TouchEventType.SWIPE,GetRaycastHit(position));
 	}
 
 	private void TouchEnd (Vector2 position) {
-
+		ProcessTouchEvent(TouchEventType.TOUCH_END,GetRaycastHit(position));
 	}
 
 	private void LongPress (Vector2 position, float pressTime) {
-
+		ProcessTouchEvent(TouchEventType.LONG_PRESS,GetRaycastHit(position));
 	}
 
 	private void Tap (Vector2 position) {
-
+		ProcessTouchEvent(TouchEventType.TAP,GetRaycastHit(position));
 	}
 
 	private void LongTap (Vector2 position, float tapTime) {
-
+		ProcessTouchEvent(TouchEventType.LONG_TAP,GetRaycastHit(position));
 	}
 
 	private void SwipeEnd (Vector2 position,Vector2 startPosition,float swipeTime) {
-	
+		ProcessTouchEvent(TouchEventType.SWIPE_END,GetRaycastHit(position));
 	}
 
-	private void ProcessTouchEvent (TouchEventType type, GameObject obj){
+	private void ProcessTouchEvent (TouchEventType touchEventType, GameObject obj){
 		if(obj == null) return;
 
-		if(_currentEvent.ContainsKey(obj)){
+		TouchEventHandlerType prevHandlerEventType = TouchEventHandlerType.NONE;
+
+		if(_prevEventDict != null && _prevEventDict.ContainsKey(obj)){
+			prevHandlerEventType = _prevEventDict[obj];
+		}
+
+		if(_currentEventDict != null && _currentEventDict.ContainsKey(obj)){
 			// TODO すでに現在のフレームで別のタッチポイントがオブジェクトを指定した時の処理を書く。
 		}else{
-			if(_currentEvent == null){
-				_currentEvent = new Dictionary<GameObject, TouchEventType>();
+			if(_currentEventDict == null){
+				_currentEventDict = new Dictionary<GameObject, TouchEventHandlerType>();
 			}
-			_currentEvent.Add(obj,type);
+		}
+
+		switch(touchEventType){
+		case TouchEventType.TOUCH:{
+			_currentEventDict[obj] = TouchEventHandlerType.TOUCH;
+			if(OnTouch != null) OnTouch(obj);
+		}break;
+
+		case TouchEventType.SWIPE:{
+			if(prevHandlerEventType == TouchEventHandlerType.SWIPE_ENTER || prevHandlerEventType == TouchEventHandlerType.SWIPE_STAY){
+				_currentEventDict[obj] = TouchEventHandlerType.SWIPE_STAY;
+				if(OnSwipeStay != null) OnSwipeStay(obj);
+			}else{
+				_currentEventDict[obj] = TouchEventHandlerType.SWIPE_ENTER;
+				if(OnSwipeEnter != null) OnSwipeEnter(obj);
+			}
+		}break;
+
 		}
 
 	}
 
-	private GameObject GetRayCastHit (Vector2 position){
+	private void ProcessAfterTouchEvent () {
+		if(_prevEventDict != null){
+			foreach(GameObject obj in _prevEventDict.Keys){
+				if(!_currentEventDict.ContainsKey(obj)){
+					// 現在のフレームで処理をされなかったものに対して処理
+					TouchEventHandlerType prevHandlerEventType = _prevEventDict[obj];
+
+					switch(prevHandlerEventType){
+					case TouchEventHandlerType.SWIPE_ENTER:{
+						if(OnSwipeExit != null) OnSwipeExit(obj);
+					}break;
+
+					case TouchEventHandlerType.SWIPE_STAY:{
+						if(OnSwipeExit != null) OnSwipeExit(obj);
+					}break;
+
+					}
+
+				}
+			}
+		}
+	}
+
+	private GameObject GetRaycastHit (Vector2 position){
 
 		Ray ray = touchCamera.ScreenPointToRay(position);
 		RaycastHit hit;
-		RaycastHit2D hit2D;
+		//RaycastHit2D hit2D;
+		Collider2D collider2D;
 		GameObject obj3D = null;
 		GameObject obj2D = null;
 
@@ -107,10 +174,16 @@ public class TouchEventHandler : MonoBehaviour {
 		}
 
 		// 2D
+		collider2D = Physics2D.OverlapPoint((Vector2)touchCamera.ScreenToWorldPoint(position));
+		if(collider2D != null){
+			obj2D = collider2D.gameObject;
+		}
+		/* なんでかよくわからないけど、null pointer が返ってくるのでコメントアウト : Physics2D.OverlapPointを使う実装に変更
 		hit2D = Physics2D.Raycast(ray.origin,ray.direction);
 		if(hit2D != null){
 			obj2D = hit2D.collider.gameObject;
 		}
+		*/
 
 		if(obj3D != null && obj2D != null){
 			float distance3D = Vector3.Distance(touchCamera.transform.position,obj3D.transform.position);
